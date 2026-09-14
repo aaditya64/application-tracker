b2ed69f02e75a11613d783ad74dc2a0810bdee90
aaditya64 <nandakishoraaditya@gmail.com>
2026-09-15 00:59:32 +0100

    basic backend

---
+++ backend/src/test/java/com/gradapp/tracker/ApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradapp.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
